0136ba7a156d4257c91495f0e0fb7ee60c6f50ac
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-07 11:42:07 +0800

    eclass

--- src/org/aaabbb/e/Document.java
+++ src/org/aaabbb/e/Document.java
@@ -4,13 +4,13 @@ import org.eclipse.jface.text.IDocument;
 
 public class Document extends Any
 {
-	public IDocument IDocument;
-	
-	public DocumentJob Job;
-	
-	public DocumentListener Listener;
-	
-	public DocumentLoad Load;
-	
-	public OutlinePage OutlinePage;
+    public IDocument IDocument;
+    
+    public DocumentJob Job;
+    
+    public DocumentListener Listener;
+    
+    public DocumentLoad Load;
+    
+    public OutlinePage OutlinePage;
 }
