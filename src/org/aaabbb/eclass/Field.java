05a94151b03911f298085a146676eb79e7deda07
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-07 15:34:05 +0800

    eclass

--- src/org/aaabbb/eclass/Field.java
+++ src/org/aaabbb/eclass/Field.java
@@ -2,11 +2,11 @@ package org.aaabbb.eclass;
 
 public class Field extends Any
 {
-	public String Class;
-	
+    public String Class;
+
     public String Name;
-	
-	public int Count;
-	
-	public Class Parent;
+
+    public int Count;
+
+    public Class Parent;
 }
