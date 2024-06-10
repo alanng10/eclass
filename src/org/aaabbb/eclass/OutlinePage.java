2bec775fb1ea09902f0f3a45e3e70061c84dc037
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-10 15:57:19 +0800

    eclass

--- src/org/aaabbb/eclass/OutlinePage.java
+++ src/org/aaabbb/eclass/OutlinePage.java
@@ -51,84 +51,61 @@ public class OutlinePage extends ContentOutlinePage
     
     public boolean Update()
     {
-        Log.This().Info("OutlinePage.Update 1111");
-
         TreeViewer viewer;
         viewer = this.getTreeViewer();
 
-        Log.This().Info("OutlinePage.Update 2222");
-
         if (viewer == null)
         {
-        	Log.This().Info("OutlinePage.Update 3333");
         	return true;
         }
         
-        Log.This().Info("OutlinePage.Update 4444");
-        
         Root a;
         a = this.Document().Load().Root();
+
+        Object k;
+        k = this.Data(a);
+        
+        viewer.setInput(k);
+        return true;
+    }
+    
+    protected Object Data(Root root)
+    {
         Object k;
         k = null;
-        if (!(a == null))
+        if (!(root == null))
         {
-        	Log.This().Info("OutlinePage.Update 5555");
-        	k = a.Class();
+        	k = root.Class();
         }
-        
-        Log.This().Info("OutlinePage.Update 6666");
-        viewer.setInput(k);
-
-        Log.This().Info("OutlinePage.Update 7777");
-        return true;
+        Object a;
+        a = k;
+        return a;
     }
 
     public void createControl(Composite parent)
     {
-        Log.This().Info("OutlinePage.createControl 1111");
-
         super.createControl(parent);
-
-        Log.This().Info("OutlinePage.createControl 2222");
 
         TreeContentProvider aa;
         aa = new TreeContentProvider();
 
-        Log.This().Info("OutlinePage.createControl 3333");
-
         TreeLabelProvider ab;
         ab = new TreeLabelProvider();
-
-        Log.This().Info("OutlinePage.createControl 4444");
 
         TreeViewer viewer;
         viewer = this.getTreeViewer();
 
-        Log.This().Info("OutlinePage.createControl 5555");
-
         viewer.setContentProvider(aa);
-
-        Log.This().Info("OutlinePage.createControl 6666");
 
         viewer.setLabelProvider(ab);
 
-        Log.This().Info("OutlinePage.createControl 7777");
-
         viewer.addSelectionChangedListener(this);
-
-        Log.This().Info("OutlinePage.createControl 8888");
 
         Root a;
         a = this.Document().Load().Root();
         Object k;
-        k = null;
-        if (!(a == null))
-        {
-        	Log.This().Info("OutlinePage.createControl 9999");
-        	k = a.Class();
-        }
-        viewer.setInput(k);
+        k = this.Data(a);
         
-        Log.This().Info("OutlinePage.createControl AAAA");
+        viewer.setInput(k);        
     }
 }
