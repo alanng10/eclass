8039a97d886a030e4e5c60c063ca551f0e600b7c
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-10 15:59:45 +0800

    eclass

--- src/org/aaabbb/eclass/PartListener.java
+++ src/org/aaabbb/eclass/PartListener.java
@@ -31,8 +31,6 @@ public class PartListener extends Any implements IPartListener2
 
         if (!(o == null))
         {
-            Log.This().Info("Class Source Document Opened");
-
             Infra infra;
             infra = Plugin.This().Infra();
 
@@ -50,26 +48,18 @@ public class PartListener extends Any implements IPartListener2
         return true;
     }
 
-    @Override
     public void partOpened(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Opened");
-
         this.ClassDocumentNew(partRef);
     }
 
-    @Override
     public void partClosed(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Closed");
-
         IDocument o;
         o = this.ClassIDocument(partRef);
 
         if (!(o == null))
         {
-            Log.This().Info("Class Source Document Closed");
-
             Document a;
             a = Plugin.This().DocumentTable().get(o);
             
@@ -87,39 +77,27 @@ public class PartListener extends Any implements IPartListener2
         }
     }
 
-    @Override
     public void partInputChanged(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Input Changed");
     }
 
-    @Override
     public void partVisible(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Visible");
     }
 
-    @Override
     public void partHidden(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Hidden");
     }
 
-    @Override
     public void partDeactivated(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Deactivated");
     }
 
-    @Override
     public void partBroughtToTop(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Brought To Top");
     }
 
-    @Override
     public void partActivated(IWorkbenchPartReference partRef)
     {
-        Log.This().Info("Part Activated");
     }
 }
