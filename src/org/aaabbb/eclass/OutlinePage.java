e2eb29f2eca44a8d8472af1c5df54be188b4227a
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-12 14:21:02 +0800

    eclass

--- src/org/aaabbb/eclass/OutlinePage.java
+++ src/org/aaabbb/eclass/OutlinePage.java
@@ -1,13 +1,10 @@
 package org.aaabbb.eclass;
 
-import org.eclipse.jface.viewers.ISelection;
-import org.eclipse.jface.viewers.ISelectionChangedListener;
+import org.eclipse.jface.viewers.TreeViewer;
 import org.eclipse.swt.widgets.Composite;
-import org.eclipse.swt.widgets.Control;
-import org.eclipse.ui.IActionBars;
-import org.eclipse.ui.views.contentoutline.IContentOutlinePage;
+import org.eclipse.ui.views.contentoutline.ContentOutlinePage;
 
-public class OutlinePage implements IContentOutlinePage
+public class OutlinePage extends ContentOutlinePage
 {
     public boolean Init()
     {
@@ -19,76 +16,98 @@ public class OutlinePage implements IContentOutlinePage
         this.dispose();
         return true;
     }
-
-    protected OutlineViewer Viewer;
     
-    @Override
+    public void dispose() {
+        super.dispose();
+    
+        this.IsFinalSet(true);
+    }
+    
+    public boolean IsFinal()
+    {
+        return this.IsFinal_D;
+    }
+    
+    public boolean IsFinalSet(boolean value)
+    {
+        this.IsFinal_D = value;
+        return true;
+    }
+   
+    protected boolean IsFinal_D;
+    
+    public Document Document()
+    {
+        return this.Document_D;
+    }
+    
+    public boolean DocumentSet(Document value)
+    {
+        this.Document_D = value;
+        return true;
+    }
+   
+    protected Document Document_D;
+    
+    public boolean Update()
+    {
+        TreeViewer viewer;
+        viewer = this.getTreeViewer();
+
+        if (viewer == null)
+        {
+            return true;
+        }
+        
+        Root a;
+        a = this.Document().Load().Root();
+
+        Object k;
+        k = this.Data(a);
+        
+        viewer.setInput(k);
+        return true;
+    }
+    
+    protected Object Data(Root root)
+    {
+        Object k;
+        k = null;
+        if (!(root == null))
+        {
+            k = root.Class();
+        }
+        Object a;
+        a = k;
+        return a;
+    }
+
     public void createControl(Composite parent)
     {
-        this.Viewer = new OutlineViewer();
-        this.Viewer.ParentSet(parent);
-        this.Viewer.Init();
-    }
+        super.createControl(parent);
 
-    @Override
-    public void dispose()
-    {
-        if (this.Viewer == null)
-        {
-            return;
-        }
-        this.Viewer.Final();
-    }
+        TreeContentProvider aa;
+        aa = new TreeContentProvider();
+        aa.Init();
 
-    @Override
-    public Control getControl()
-    {
-        if (this.Viewer == null)
-        {
-            return null;
-        }
-        return this.Viewer.getControl();
-    }
-
-    @Override
-    public void setActionBars(IActionBars actionBars)
-    {
-        // TODO Auto-generated method stub
+        TreeLabelProvider ab;
+        ab = new TreeLabelProvider();
+        ab.Init();
         
-    }
+        TreeViewer viewer;
+        viewer = this.getTreeViewer();
 
-    @Override
-    public void setFocus()
-    {
-        return;
-    }
+        viewer.setContentProvider(aa);
 
-    @Override
-    public void addSelectionChangedListener(ISelectionChangedListener listener)
-    {
-        // TODO Auto-generated method stub
+        viewer.setLabelProvider(ab);
+
+        viewer.addSelectionChangedListener(this);
+
+        Root a;
+        a = this.Document().Load().Root();
+        Object k;
+        k = this.Data(a);
         
+        viewer.setInput(k);        
     }
-
-    @Override
-    public ISelection getSelection()
-    {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
-    public void removeSelectionChangedListener(ISelectionChangedListener listener)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-
-    @Override
-    public void setSelection(ISelection selection)
-    {
-        // TODO Auto-generated method stub
-        
-    }
-    
 }
