ef46e8dc1e7f0da8dd410b585726a8f3e947035f
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-15 14:51:29 +0800

    eclass

--- src/org/alanng1/eclass/Document.java
+++ src/org/alanng1/eclass/Document.java
@@ -7,79 +7,79 @@ public class Document extends Any
 {
     public IDocument IDocument()
     {
-    	return this.IDocument_D;
+        return this.IDocument_D;
     }
-    
+
     public boolean IDocument(IDocument value)
     {
-    	this.IDocument_D = value;
-    	return true;
+        this.IDocument_D = value;
+        return true;
     }
-    
+
     protected IDocument IDocument_D;
-    
+
     public ITextEditor Editor()
     {
         return this.Editor_D;
     }
-    
+
     public boolean Editor(ITextEditor value)
     {
         this.Editor_D = value;
         return true;
     }
-    
-    protected ITextEditor Editor_D;    
-    
+
+    protected ITextEditor Editor_D;
+
     public DocumentJob Job()
     {
-    	return this.Job_D;
+        return this.Job_D;
     }
-    
+
     public boolean Job(DocumentJob value)
     {
-    	this.Job_D = value;
-    	return true;
+        this.Job_D = value;
+        return true;
     }
-    
+
     protected DocumentJob Job_D;
-    
+
     public DocumentListener Listener()
     {
-    	return this.Listener_D;
+        return this.Listener_D;
     }
-    
+
     public boolean Listener(DocumentListener value)
     {
-    	this.Listener_D = value;
-    	return true;
+        this.Listener_D = value;
+        return true;
     }
-    
+
     protected DocumentListener Listener_D;
-    
+
     public DocumentLoad Load()
     {
-    	return this.Load_D;
+        return this.Load_D;
     }
-    
+
     public boolean Load(DocumentLoad value)
     {
-    	this.Load_D = value;
-    	return true;
+        this.Load_D = value;
+        return true;
     }
-    
+
     protected DocumentLoad Load_D;
-    
+
     public OutlinePage OutlinePage()
     {
-    	return this.OutlinePage_D;
+        return this.OutlinePage_D;
     }
-    
+
     public boolean OutlinePage(OutlinePage value)
     {
-    	this.OutlinePage_D = value;
-    	return true;
+        this.OutlinePage_D = value;
+        return true;
     }
-    
+
     protected OutlinePage OutlinePage_D;
 }
