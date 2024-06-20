478b3c86c72cf49155c1caa07f7ec34e251a6581
Alan Ng <aaabbbcccddd00001111@gmail.com>
2024-06-20 16:35:14 +0800

    eclass

--- src/org/alanng10/eclass/ConfigIndex.java
+++ src/org/alanng10/eclass/ConfigIndex.java
@@ -6,11 +6,29 @@ public class ConfigIndex extends Any
     {
         super.Init();
         
+        this.ServerFilePath_D = "Server.FilePath";
+        
+        this.NetworkPortServer_D = "Network.PortServer";
+        
         this.OutlineSort_D = "Outline.Sort";
         
         this.OutlineKind_D = "Outline.Kind";
         return true;
     }
+
+    public String ServerFilePath()
+    {
+        return this.ServerFilePath_D;
+    }
+    
+    protected String ServerFilePath_D;
+
+    public String NetworkPortServer()
+    {
+        return this.NetworkPortServer_D;
+    }
+    
+    protected String NetworkPortServer_D;
     
     public String OutlineSort()
     {
